2dfb2934a0913b47c7d05581643364544a890655
Jason Hartley <jasonhartley@gmail.com>
2015-06-18 01:14:08 -0400

    created empty SearchActivity and ResultsActivity

---
+++ app/src/main/java/us/jasonh/spotifystreamer/ResultsActivityFragment.java
@@ -0,0 +1,22 @@
+package us.jasonh.spotifystreamer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ResultsActivityFragment extends Fragment {
+
+    public ResultsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_results, container, false);
+    }
+}
---
+++ app/src/main/java/us/jasonh/spotifystreamer/SearchActivityFragment.java
@@ -0,0 +1,22 @@
+package us.jasonh.spotifystreamer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SearchActivityFragment extends Fragment {
+
+    public SearchActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
